4d1fa0fa668fb8bd07055a8b91525df907ca38fb
kangckck23@gmail.com <1qaz1974>
2020-10-07 16:31:47 +0900

    Initial commit

---
+++ src/test/java/org/zerock/ex1/Ex1ApplicationTests.java
@@ -0,0 +1,17 @@
+package org.zerock.ex1;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Log4j2
+class Ex1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+        log.info("contextLoads...");
+    }
+
+}
